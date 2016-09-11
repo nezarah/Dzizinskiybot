a6ce76c9100ba5f8e2a98bcb958e69d49e2f9d85
Nazar <cozak.nazar@gmail.com>
2016-09-11 19:47:17 +0300

    commit

--- src/DzizinskiyLaunch.java
+++ src/DzizinskiyLaunch.java
@@ -1,4 +1,5 @@
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Random;
 
@@ -9,10 +10,10 @@ import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
- 
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+
 public class DzizinskiyLaunch extends TelegramLongPollingBot {
-	
- 
+
 	public static void main(String[] args) {
 		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 		try {
@@ -21,83 +22,59 @@ public class DzizinskiyLaunch extends TelegramLongPollingBot {
 			e.printStackTrace();
 		}
 	}
-	 
-		@Override
-		public String getBotUsername() {
-			return "Dzizinskiybot";
-		}
-	 
-		@Override
-		public String getBotToken() {
-			return "283093949:AAFwn-fM4P7ndMhntYfc_I2bD4jfRQhXOvY";
-		}
-	
-	String[][] dimaChat={
-			//стандартне привітання
-			{"привіт","хай","ку"},
-			{"привіт, як тебе звати?☺️","привіт, ти фрешка?☺️","хаю хай☺️"},
-			//справи
-			{"як справи","як в тебе справи","як ти"},
-			{"чудово, щойно з Ауруму прийшов","та готуюсь до Міхалевича"},
-			//років
-			{"Скільки тобі років?"},
-			{"Я вже у мами дорослий — мені 20"},
-			//різні ситуації
-			{"навчання"},
-			{"та забий на навчання"},
-			//різні ситуації
-			{"кораблик"},
-			{"о, пішли зі мною на кораблик"},
-			//різні ситуації
-			{"коли ти будеш вчитись?"},
-			{"ой, ти знову про це"},
-			//різні ситуації
-			{"як тебе звуть"},
-			{"ой, ти знову про це"},
-			//років
-			{"Глибовець"},
-			{"Найкращий викладач у світі☺️"},
-			//дефолт
-			{"",
-			"Полайкай мені фотки в інстаграмі @dzizinskiy", "Твої батьки не програмісти? тоді звідки у них така фіча"}
-		};
-	
-	
-	private String timeNow() {
-		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		String time = sdf.format(cal.getTime()) ;
-		return time;
-	}
-	
-	private String universityGo() {
-		String words = "Йти:Не йти:Дивлячись чи є Міхалевич";
-		String[] wordsAsArray = words.split(":");	
-		int index = new Random().nextInt(wordsAsArray.length);
-		String goOrNotToGo = wordsAsArray[index];
-		return goOrNotToGo;
+
+	@Override
+	public String getBotUsername() {
+		return "Dzizinskiybot";
 	}
 
+	@Override
+	public String getBotToken() {
+		return "283093949:AAFwn-fM4P7ndMhntYfc_I2bD4jfRQhXOvY";
+	}
+
+	String[][] dimaChat = {
+			//привітання
+			{ "прив", "хай", "ку", "здоров", "добр" },
+			{ "привіт, я Діма, як тебе звати?☺️", "привіт, я Діма, ти фрешка?☺️", "хаю хай☺️" },
+			// справи
+			{ "справи", "почуваєшся", "робиш" , "робити"},
+			{ "чудово, щойно з Ауруму прийшов", "та готуюсь до Міхалевича" },
+			// років
+			{ "рік", "років" }, { "Я вже у мами дорослий — мені 20" },
+			// різні ситуації
+			{ "навчання" }, { "та забий на навчання" },
+			// різні ситуації
+			{ "кораблик" }, { "о, пішли зі мною на кораблик" },
+			// різні ситуації
+			{ "коли" }, { "ой, ти знову про це" },
+			{ "глибовець" }, { "Найкращий викладач у світі☺️" },
+			// дефолт
+			{"Полайкай мені фотки в інстаграмі @dzizinskiy",
+					"Твої батьки не програмісти? тоді звідки у них така фіча" } };
 	
 	@Override
 	public void onUpdateReceived(Update update) {
 		Message message = update.getMessage();
 		if (message != null && message.hasText()) {
-			if (message.getText().equals("/допомога")){
-				sendMsg(message, "Привіт, я Діма ☺️");
-			}
-			else if (message.getText().contains("година")){
-				sendMsg(message, timeNow());
-			}
-			else if (message.getText().equals("/пари")){
-				sendMsg(message, universityGo());
-			} else {
-				sendMsg(message, "Полайкай мені фотки в інстаграмі @dzizinskiy");
-			} 
 			
+			String msg = message.getText().toLowerCase();
+			String[] parts = msg.split(" ");
+			
+			for (int p = 0; p <= parts.length; p++){
+				for(int i = 0; i < dimaChat.length; i+=2){
+					for(int j = 0; j < dimaChat[i].length; j++){
+						if(parts[p].contains(dimaChat[i][j])){
+							int k = new Random().nextInt(dimaChat[i+1].length);
+							sendMsg(message, dimaChat[i+1][k]);
+						}
+					}
+				}
+			}
+			sendMsg(message, "def");
 		}
 	}
- 
+
 	private void sendMsg(Message message, String text) {
 		SendMessage sendMessage = new SendMessage();
 		sendMessage.enableMarkdown(true);
@@ -110,5 +87,5 @@ public class DzizinskiyLaunch extends TelegramLongPollingBot {
 			e.printStackTrace();
 		}
 	}
- 
+
 }
